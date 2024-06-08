c0b2ee207a193a57cde7f63c0367f68f29ac678e
vasantha ratnam <suryasabbavarapu248@gmail.com>
2024-06-08 21:45:57 +0530

    end

--- frontend/Tour/src/Components/Header/Header.jsx
+++ frontend/Tour/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useEffect , useRef}from 'react'
 import  {Container ,  Row , Button} from 'react-bootstrap'
 import { NavLink ,  Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
@@ -22,8 +22,28 @@ const nav__links = [
 
 
 function Header() {
+
+   const headerRef = useRef(null);
+    
+   const stickyHeaderFunc = () =>{
+      window.addEventListener('scroll' , () => {
+         if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+            headerRef.current.classList.add('sticky__header');
+         }
+         else{
+           headerRef.current.classList.remove('sticky__header');
+         }
+      })
+   }
+
+   useEffect(() => {
+       stickyHeaderFunc()
+
+       return () => window.removeEventListener('scroll' , stickyHeaderFunc)
+   })
+
   return (
-    <header className = "header">
+    <header className = "header"  ref = {headerRef}>
      <Container>
         <Row>
            <div className = "nav__wrapper d-flex align-items-center justify-content-between">
